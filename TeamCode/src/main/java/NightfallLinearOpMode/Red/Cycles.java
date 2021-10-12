7e31d96b1250b47e31899e543537f0b0d4082075
mrjmac <40571030+mrjmac@users.noreply.github.com>
2021-10-11 23:58:28 -0500

    basic auto files
    maybe waitforstart();

---
+++ TeamCode/src/main/java/NightfallLinearOpMode/Red/Cycles.java
@@ -0,0 +1,46 @@
+package NightfallLinearOpMode.Red;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import NightfallLinearOpMode.Intake;
+import NightfallLinearOpMode.Drivetrain;
+import NightfallLinearOpMode.Lift;
+import NightfallLinearOpMode.Vision;
+
+@Autonomous(name = "Red Cycles", group = "LinearOpMode")
+public class Cycles extends LinearOpMode {
+
+    private Drivetrain drivetrain;
+    private Vision vision;
+    private Lift lift;
+    private Intake intake;
+    private String pos;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drivetrain = new Drivetrain(this);
+        vision = new Vision(this);
+        lift = new Lift(this);
+        intake = new Intake(this);
+
+        while (!isStarted()) {
+            pos = vision.getTeamMarkerPos();
+            telemetry.addData("team marker pos: ", pos);
+            telemetry.update();
+        }
+        telemetry.addData("team marker pos: ", pos);
+        telemetry.update();
+
+        //trajectories/pose go here
+
+        idle();
+
+
+
+
+
+    }
+}
