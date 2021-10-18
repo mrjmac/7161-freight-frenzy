6800e5c26c622a4ca28ece62f5d1a38f8fd1c31f
mrjmac <40571030+mrjmac@users.noreply.github.com>
2021-10-17 22:11:29 -0500

    pseudocode for duck autos
    just change distances/import dt functions

--- TeamCode/src/main/java/NightfallLinearOpMode/Red/Duck.java
+++ TeamCode/src/main/java/NightfallLinearOpMode/Red/Duck.java
@@ -41,8 +41,59 @@ public class Duck extends LinearOpMode {
 
         idle();
 
+//======================= DEPOSIT MINERAL AND INTAKE TEAM MARKER =========================================
+        //drivetrain.move(12);
+        if (pos.equals("1")) {
+            //lift.setheight(1);
+            //lift.drop;
+            //lift.setheight(0);
+            //drivetrain.turn(-90);
+            //intake.intake(5);
+            //drivetrain.turn(90);
+        }
+        else if (pos.equals("2")) {
+            //lift.setheight(2);
+            //lift.drop;
+            //lift.setheight(0);
+            //drivetrain.turn(-90);
+            //drivetrain.move(6);
+            //intake.intake(5);
+            //drivetrain.move(-6);
+            //drivetrain.turn(90);
+        }
+        else {
+            //lift.setheight(2);
+            //lift.drop;
+            //lift.setheight(0);
+            //drivetrain.turn(-90);
+            //drivetrain.move(10);
+            //intake.intake(5);
+            //drivetrain.move(-10);
+            //drivetrain.turn(90);
+        }
+//======================= DUCK =========================================
+        //drivetrain.move(-10);
+        //drivetrain.turn(90);
+        //drivetrain.move(-18);
+        //drivetrain.duck(4);
+        //drivetrain.turn(-45);
+        //intake.intake(5);
 
-
+//======================= PARK =========================================
+        //drivetrain.turn(-45);
+        //drivetrain.move(60);
+        //drivetrain.turn(90);
+        //drivetrain.move(90);
+        //drivetrain.turn(90);
+        //drivetrain.move(52);
+        //drivetrain.turn(-90);
+        //TODO: add odom retract
+        //drivetrain.forwards(48);
+        //drivetrain.turn(90)
+        //drivetrain.forwards(12);
+//======================= COLLECT MINERAL AND TURN =========================================
+        //intake.intake(5);
+        //drivetrain.turn(90);
 
 
     }
