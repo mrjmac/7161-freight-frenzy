785264fa4d9d77d8ef6960df861cc024959a0124
Jeff <43101432+jeffffu@users.noreply.github.com>
2022-02-25 01:40:51 -0600

    duck consistent blue

--- TeamCode/src/main/java/NightfallLinearOpMode/Red/DuckV2.java
+++ TeamCode/src/main/java/NightfallLinearOpMode/Red/DuckV2.java
@@ -1,4 +1,4 @@
-package NightfallLinearOpMode.Blue;
+package NightfallLinearOpMode.Red;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -8,7 +8,7 @@ import NightfallLinearOpMode.Drivetrain;
 import NightfallLinearOpMode.Lift;
 import NightfallLinearOpMode.Vision;
 
-@Autonomous(name = "Blue DuckV2", group = "blue")
+@Autonomous(name = "Red DuckV2", group = "blue")
 public class DuckV2 extends LinearOpMode {
 
     private Drivetrain drivetrain;
@@ -39,13 +39,6 @@ public class DuckV2 extends LinearOpMode {
 
         while (!isStarted()) {
             pos = vision.getPosNewMethod();
-            if (pos == 1)
-                pos = 2;
-            else if
-            (pos == 2)
-                pos = 3;
-            else
-                pos = 1;
             telemetry.addData("team marker pos: ", pos);
             telemetry.update();
         }
@@ -63,20 +56,20 @@ public class DuckV2 extends LinearOpMode {
 
      */
         drivetrain.gyroEncoderInch(1, 15, 1.5, 0);
-        drivetrain.turnPD(15, kpTurn5, kdTurn5, 2);
-        drivetrain.gyroEncoderInch(.3, 39, 1.5, 15);
-        drivetrain.gyroEncoderInch(-1, 21, 1.5, 15);
-     //   lift.capDown();
+        drivetrain.turnPD(-15, kpTurn5, kdTurn5, 2);
+        drivetrain.gyroEncoderInch(.3, 39, 1.5, -15);
+        drivetrain.gyroEncoderInch(-1, 21, 1.5, -15);
+        //   lift.capDown();
         //intake.intakeDown();
-    //    sleep(300);
-   //     lift.capUp();
-        drivetrain.turnPD(-46, kpTurn45, kdTurn45, 1.5);
-        drivetrain.gyroEncoderInch(1, 15, 2, -46);
+        //    sleep(300);
+        //     lift.capUp();
+        drivetrain.turnPD(46, kpTurn45, kdTurn45, 1.5);
+        drivetrain.gyroEncoderInch(1, 15, 2, 46);
         lift.setLift(liftHeight, 1);
         // CAROUSEL, PICK UP DUCK AND SCORE
-        drivetrain.gyroEncoderInch(-.5, 34, 2.7, -55);
-        drivetrain.turnPD(-22.5, kpTurn45, kdTurn45, 2);
-        drivetrain.gyroEncoderInch(-.5, 15.5, 1, -25);
+        drivetrain.gyroEncoderInch(-.5, 34, 2.7, 55);
+        drivetrain.turnPD(22.5, kpTurn45, kdTurn45, 2);
+        drivetrain.gyroEncoderInch(-.5, 15.5, 1, 25);
         drivetrain.duckStart(-1);
         sleep(4500);
         intake.goatIntake(.7);
@@ -85,18 +78,18 @@ public class DuckV2 extends LinearOpMode {
         drivetrain.duckStop();
         sleep(1000);
         intake.goatIntake(0);
-        drivetrain.gyroEncoderInch(1, 8, 1.3, -20);
+        drivetrain.gyroEncoderInch(1, 8, 1.3, 20);
         //intake.goatIntake(0);
         drivetrain.turnPD(0, kpTurn5, kdTurn5, 1);
         intake.goatIntake(.7);
         drivetrain.gyroEncoderInch(-1, 9, 1, 0);
         drivetrain.gyroEncoderInch(1, 20, 2, 0);
         intake.goatIntake(0);
-        drivetrain.turnPD(-70, kpTurn45, kpTurn45, 2);
-        drivetrain.gyroEncoderInch(1, 24, 1.5, -70);
+        drivetrain.turnPD(70, kpTurn45, kpTurn45, 2);
+        drivetrain.gyroEncoderInch(1, 24, 1.5, 70);
         lift.setLift(4, 1);
         //drivetrain.turnPD(-90, kpTurn45, kdTurn45, 2);
-        drivetrain.gyroEncoderInch(-1, 31, 3, -80);
-        drivetrain.turnPD(-90, kpTurn5, kdTurn5, 1);
+        drivetrain.gyroEncoderInch(-1, 31, 3, 80);
+        drivetrain.turnPD(90, kpTurn5, kdTurn5, 1);
     }
 }
